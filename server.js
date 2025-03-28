20876a1593b2e0998f355ea4063d455aa818e7c1
Qisanet Angesom <ksanetang@gmail.com>
2025-03-28 09:53:20 +0300

    loppp

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 
 // Enable CORS
 app.use(cors({
-  origin: 'https://portfolio-frontend-inky-five.vercel.app', // Ensure this matches your frontend
+  origin: '*', // Ensure this matches your frontend
   methods: 'GET,POST,PUT,DELETE,OPTIONS',
   allowedHeaders: 'Content-Type, Authorization',
   credentials: true
